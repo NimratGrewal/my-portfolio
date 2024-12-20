7b468e0e2d8b75b348e34345ed21135cea758c13
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2024-12-20 03:13:41 -0500

    changes to backgroun, adding new about me seciiotn

--- components/cards/project_card.tsx
+++ components/cards/project_card.tsx
@@ -6,33 +6,34 @@ interface ProjectInputProps {
     desc: string;
     image: string;
     className?: string;
+    cardDetails?: string;
     onClick: (value: string) => void;
     tags: string[];
 }
 
-const ProjectCard: React.FC<ProjectInputProps> = ({desc, title, className, onClick, image, tags}) => {
+const ProjectCard: React.FC<ProjectInputProps> = ({desc, title, cardDetails, className, onClick, image, tags}) => {
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         onClick(event.target.value);
     };
 
     return (
  
-        <div className={`flex flex-col bg-white dark:bg-gray-800 rounded-3xl shadow-lg hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg ${className}`}>
-            <div className="w-full h-56 overflow-hidden"><img className="w-full h-full object-cover" src={image} alt={title} /></div>
+        <div className={`${cardDetails} flex flex-col bg-gray-50 dark:bg-gray-800 rounded-lg shadow-xl hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 max-w-xl`}>
+            <div className="w-full h-56 overflow-hidden"><img className="w-full h-full object-cover border border-gray-200 dark:border-gray-800" src={image} alt={title} /></div>
             
             {/* Content area */}
             <div className="p-6">
                 <h2 className="text-2xl font-semibold text-black dark:text-white mb-4">
                 {title}
                 </h2>
-                <p className="text-gray-600 dark:text-gray-400 mb-4">
+                <p className="text-gray-700 dark:text-gray-300 mb-4">
                 {desc}
                 </p>
                 <div className="flex flex-wrap gap-2">
                 {tags.map((tag, index) => (
                     <span
                     key={index}
-                    className="bg-gradient-to-r from-indigo-500 to-indigo-700 text-white text-sm font-semibold px-2 py-1 rounded-full dark:bg-indigo-100 dark:text-white"
+                    className={`${className} bg-indigo-700 text-white text-sm font-semibold px-2 py-1 rounded-full dark:bg-indigo-100 dark:text-black`}
                     >
                     {tag}
                     </span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -55,34 +55,29 @@ export default function Home() {
           <div className="flex flex-col text-center lg:text-left">
             <div>
             <h1 className="text-black dark:text-white text-3xl md:text-4xl font-bold leading-relaxed">
-              Hi! I'm Nimrat. <br />
+              Hi! I'm Nimrat. <br />  <br></br>
               <span className="text-indigo-600 dark:text-indigo-400">
-                An aspiring Software Engineer
+                Aspiring Software Engineer. <br></br>
               </span>
               <br />
-              passionate about creating software solutions.
+              Driven to create software solutions.
             </h1>
-
-            <p className="mt-4 text-gray-600 dark:text-gray-400">
-              I'm currently a third year CS student at the University of Toronto.
-            </p>
-            <p className="mt-4 text-gray-600 dark:text-gray-400">
-              I love building innovative and scalable applications while focusing on clean design and functionality.
-            </p>
             </div>
             
-            <div className="mt-8 flex flex-wrap justify-center lg:justify-start gap-4">
+            <div className="mt-8 flex flex-row sm:justify-center md:justify-start lg:justify-start gap-4">
             <a 
               href="https://github.com" 
               target="_blank" 
               rel="noopener noreferrer"
-              className="m-3 p-3 bg-indigo-600 text-white dark:bg-indigo-500 shadow-lg rounded-3xl hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg"
+              className="m-2 p-3 bg-indigo-600 text-white dark:bg-indigo-500 shadow-lg rounded-3xl hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg"
             >
               Github
             </a>
-              <button className="m-3 p-3 bg-white text-black dark:text-white dark:bg-gray-800 rounded-3xl shadow-lg hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg">Linkedin</button>
-              <button className="m-3 p-3 bg-white text-black dark:text-white dark:bg-gray-800 rounded-3xl shadow-lg hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg">Download Resume</button>
+              <button className="m-2 p-3 bg-white text-black dark:text-white dark:bg-gray-800 rounded-3xl shadow-lg hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg">Linkedin</button>
+              <button className="m-2 p-3 bg-white text-black dark:text-white dark:bg-gray-800 rounded-3xl shadow-lg hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg">Download Resume</button>
+              <button className="m-2 p-3 bg-white text-black dark:text-white dark:bg-gray-800 rounded-3xl shadow-lg hover:shadow-2xl dark:shadow-gray-800 cursor-pointer overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-black dark:hover:border-gray-300  max-w-lg">Contact</button>
             </div>
+
 
           </div>
           
@@ -90,7 +85,7 @@ export default function Home() {
           <div className="flex justify-center">
             <div className="relative">
               <img
-                className="border-4 border-indigo-600 dark:border-indigo-400 h-auto object-cover w-48 sm:w-56 md:w-64 lg:w-80 rounded-full shadow-lg transform transition-all hover:scale-105 hover:shadow-2xl"
+                className="border-4 border-indigo-600 dark:border-indigo-400 h-auto object-cover w-48 sm:w-56 md:w-64 lg:w-80 rounded-full shadow-lg duration-200 transform transition-all hover:scale-105 hover:shadow-2xl"
                 src={"nimrat_pic.heic"}
                 alt="Nimrat"
               />
@@ -101,24 +96,47 @@ export default function Home() {
         </div>
       </section>
 
+      <section className="bg-gray-100">
+        <div className="flex flex-col text-center lg:text-left">
+            <div>
+            <h1 className="text-black dark:text-white text-3xl md:text-4xl font-bold leading-relaxed">
+              About me </h1>
+            <div className="text-black dark:text-white">
+              I am a current third year Computer Science student at the University of Toronto with a minor in Statistics. 
+
+              During my free time 
+            </div>
+          </div>
+          </div>
+        
+
+
+
+      </section>
+
         
         
         
-        
-      <h2 className="text-center p-4 text-black dark:text-white text-3xl md:text-3xl font-bold leading-relaxed">Projects</h2>
-        <section className="flex justify-center items-center min-h-screen">
-          <div className="grid md:grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 gap-14 sm:p-6 lg:p-8">
+      <section className="relative bg-fixed bg-[radial-gradient(#9ca3af_1px,transparent_1px)] dark:bg-[radial-gradient(#1E293B_1px,transparent_1px)] [background-size:16px_16px]">
+        <div className="flex flex-row justify-center">
+      <h2 className="text-center p-4 text-black dark:text-white text-3xl md:text-3xl font-bold leading-relaxed m-3 mt-7 bg-white/70 dark:bg-gray-800/70 backdrop-blur-sm rounded-lg shadow-md">
+              Projects
+      </h2></div>
+        <section className="flex justify-center items-center min-h-screen !z-20">
+          <div className=" grid md:grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 gap-14 sm:p-6 lg:p-8">
           <ProjectCard
-              className=""
+              className="dark:text-indigo-800"
+              cardDetails="border border-grey-900 dark:border-gray-900"
               onClick={handleClick}
               title="scriptorium - code editor"
               desc="full stack CRUD web application"
-              tags={["React.js", "Next.js","TypeScript", "JavaScript", "Prisma", "Docker", "Tailwind CSS","MVC"]} // Tags array
+              tags={["React.js", "Next.js","TypeScript", "JavaScript", "Prisma", "Docker", "jwt", "Tailwind CSS","MVC"]} // Tags array
               image="scriptorium.png"
           />
 
           <ProjectCard
-                  className=""
+                  className="dark:text-indigo-800"
+                  cardDetails="border border-grey-900 dark:border dark:border-gray-900"
                   onClick={handleClick}
                   title="access connect"
                   desc="web based AI Chatbot for University of Toronto - Scarborough Campus Accessibilty Office"
@@ -126,7 +144,8 @@ export default function Home() {
                   image="accessConnect.png"
               />
             <ProjectCard
-                    className=""
+                    className="dark:text-indigo-800"
+                    cardDetails="border border-grey-900 dark:border dark:border-gray-900"
                     onClick={handleClick}
                     title="personal portfolio"
                     desc="you are viewing this right now!"
@@ -135,16 +154,18 @@ export default function Home() {
                 />
 
         <ProjectCard
-                    className=""
+                    className="dark:text-indigo-800"
+                    cardDetails="border border-grey-900 dark:border dark:border-gray-900"
                     onClick={handleClick}
                     title="iSong"
                     desc="social music sharing desktop application"
-                    tags={["Java", "Java Swing","Spotify API", "Clean Architecture", "SOLID design principles"]} // Tags array
+                    tags={["Java","OOP", "Java Swing","Spotify API", "Clean Architecture", "SOLID design principles"]} // Tags array
                     image="207_pic.png"
                 />
 
           <ProjectCard
-                    className=""
+                    className="dark:text-indigo-800"
+                    cardDetails="border border-grey-900 dark:border dark:border-gray-900"
                     onClick={handleClick}
                     title="tetris"
                     desc="play tetris in MIPs assembly code editor"
@@ -152,7 +173,8 @@ export default function Home() {
                     image="tetris.png"
                 />
             <ProjectCard
-                    className=""
+                    className="dark:text-indigo-800"
+                    cardDetails="border border-grey-900 dark:border dark:border-gray-900"
                     onClick={handleClick}
                     title="currency converter"
                     desc="real time currency converter"
@@ -161,6 +183,7 @@ export default function Home() {
                 />
 
           </div>
+          </section>
           </section>
 
        </main>
